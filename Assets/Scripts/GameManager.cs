d069448d15a3abc8feeb84ad91b50f9b87815409
sowTecsup <jneyra@tecsup.edu.pe>
2026-03-25 08:23:31 -0500

    S2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using Sowtank.GameUtils;
 using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
@@ -8,27 +7,7 @@ public class GameManager : MonoBehaviour
     
     void Start()
     {
-        GameUtils.Process("Hello, World!", message => Debug.Log(message));
-
-
-        GameUtils.Process("Hello, World!", x => Debug.Log(x));
-
-        GameUtils.Process<string,string>("Hello, World!", "asdasda", (x,y) => Debug.Log(x + " " + y));
-
-
-        GameUtils.Process<string>("Hello, World!", x => Debug.Log(x));
-
-
-
-        GameUtils.Process("Hello, World!", message => 
-                                                        { 
-                                                            Debug.Log(message);
-                                                        });
-        List<int> numeros = new List<int> { 1, 2, 3, 4 };
-
-        // numeros.ForEach
-
-      //  InflictDamageClass.ApplyDamage(24);
+        
     }
 
     void Update()
--- Assets/Scripts/Interfaces.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IDamageable
-{
-    public void TakeDamage(int damage); 
-}
